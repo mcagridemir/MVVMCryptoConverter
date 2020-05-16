9597d426e8136b9d6a9e2f941dc21eee00866f9a
mcagridemir <cagriidemirr@gmail.com>
2020-05-17 00:10:44 +0300

    Initial Commit

---
+++ CryptoCurrency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoCurrency
+//
+//  Created by Çağrı Demir on 17.05.2020.
+//  Copyright © 2020 Cagri Demir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
