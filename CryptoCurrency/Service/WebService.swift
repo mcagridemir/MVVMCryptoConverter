8105a3385e3abaf18bb6122cf6449b44b27d7e66
mcagridemir <cagriidemirr@gmail.com>
2020-05-17 04:07:51 +0300

    commit

--- CryptoCurrency/Service/WebService.swift
+++ CryptoCurrency/Service/WebService.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+class WebService {
+    
+    func downloadCurrencies(url: URL, completion: @escaping ([CryptoCurrency]?) -> ()) { //Birden fazla oldugu icin dizi.
+        //@escaping, bir closure dan yani tamamlama blogunda escaping kullaninca fonksiyon calisinca sonunda birsey dondurmek istiyor isek kullanilir. Once donecek olan data cesidi olmali, sonra bir void donecegi yazmali.
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            }
+            else if let data = data {
+                
+                let cryptoList = try? JSONDecoder().decode([CryptoCurrency].self, from: data)
+                
+                print(cryptoList)
+                
+                if let cryptoList = cryptoList {
+                    completion(cryptoList)
+                }
+            }
+        }.resume() // islemi baslatmamiza olanak saglayan resume() dur.
+    }
+}
--- CryptoCurrency/ViewModel/CryptoViewModel.swift
+++ CryptoCurrency/ViewModel/CryptoViewModel.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+struct CryptoListViewModel {
+    let cryptoCurrencyList: [CryptoCurrency]
+}
+
+extension CryptoListViewModel {
+    
+    func numberOfRowsInSection() -> Int {
+        return self.cryptoCurrencyList.count
+    }
+    func cryptoAtIndex(_ index: Int) -> CryptoViewModel {
+        let crypto = self.cryptoCurrencyList[index]
+        return CryptoViewModel(crypto)
+    }
+}
+
+extension String {
+    func printName() {
+        print("Cagri")
+    }
+}
+
+struct CryptoViewModel {
+    let cryptoCurrency: CryptoCurrency
+
+    init(_ crypto: CryptoCurrency) {
+        self.cryptoCurrency = crypto
+    }
+}
+
+extension CryptoViewModel {
+    var name: String {
+        return self.cryptoCurrency.currency
+    }
+    
+    var price: String {
+        return self.cryptoCurrency.price
+    }
+}
